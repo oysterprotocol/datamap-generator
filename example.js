b792080b8e60d3f9ce511111aa3310439a249990
Jared Farago <jared.farago@gmail.com>
2018-04-16 09:42:12 -0400

    Initial Commit

---
+++ example.js
@@ -0,0 +1,10 @@
+import generator from './generator';
+
+const handle = "bubbles.e9a6859d2ad6b65d405d7d767fe6f8fbe5963857bb220da997669e37ad303c11rynrb09g";
+const size = 124;
+
+
+console.log(generator.generate(handle,size));
+
+
+

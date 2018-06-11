09572c61242c79a7cce61aa863711a0d8f6f7552
Rebel Fornea <rebelfornea@gmail.com>
2018-06-11 17:53:43 -0400

    cleaner exports, ignore and remove test files from distribution

--- src/services/iota.js
+++ src/services/iota.js
@@ -1,5 +1,5 @@
 import IOTA from "iota.lib.js";
-import {IOTA_API} from "../config/index";
+import { IOTA_API } from "../config/index";
 
 const Iota = new IOTA();
 const toAddress = string => string.substr(0, IOTA_API.ADDRESS_LENGTH);

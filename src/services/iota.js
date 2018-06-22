e50e2dd4e6899fce0f862537bfa0665713c08f90
Jared Farago <jared.farago@gmail.com>
2018-06-22 13:48:47 -0400

    imported iota utils only (#23)

--- src/services/iota.js
+++ src/services/iota.js
@@ -1,10 +1,9 @@
-import IOTA from "iota.lib.js";
+import utils from "iota.lib.js/lib/utils/utils";
 import { IOTA_API } from "../config/index";
 
-const Iota = new IOTA();
 const toAddress = string => string.substr(0, IOTA_API.ADDRESS_LENGTH);
 
 export default {
   toAddress,
-  utils: Iota.utils
+  utils: utils
 };
